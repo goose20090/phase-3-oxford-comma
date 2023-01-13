428c23d3aecfcef189cdb62b489d5bda467e22fc
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-01-13 18:03:48 +0000

    commit tree 64024729e0fde62ad782250c534e289ee4dc2aea

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
+require 'pry'
+# takes an array of strings
 def oxford_comma(array)
-
+    # returns a string
+    modArr = array.map.with_index do |str, index| 
+        # for all but last add comma and space to string
+        if array.length == 1
+            return str
+        elsif index == array.length - 1
+            "and #{str}"
+        elsif array.length == 2
+            str + ' '
+        else
+        str + ', '
+        end
+    end
+    modArr.join
 end
+
+# binding.pry
+# oxford_comma(["fiddleheads", "okra", "kohlrabi"])
+# => "fiddleheads, okra, and kohlrabi"
